a517524afa91a9c540880760eed815ff78c947d1
motionio <118689888+motionio@users.noreply.github.com>
2022-11-25 09:05:32 +0100

    Update app.java

--- app.java
+++ app.java
@@ -3,3 +3,5 @@ This code is subject to changes
 Please do not hard code
 Use dynamic variables
 Author: Newton Omorojor
+The idea of code optimization is always being improved upon
+Dedication is the key
